9138b52c243ee41f491f295b8d687f12659fcaea
Arunkumar Thangaraj <athangaraj@ets.org>
2021-02-09 08:34:57 +0530

    Initial commit

---
+++ src/test/java/com/suite/cases/DemoTest.java
@@ -0,0 +1,15 @@
+package com.suite.cases;
+
+import org.testng.annotations.Test;
+import org.testng.Assert;
+
+public class DemoTest {
+	
+	@Test
+	public void loginwolvi() {
+		System.out.println("wolverine");
+		Assert.assertEquals("1", "1");
+	}
+	
+
+}
